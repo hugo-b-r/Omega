fbdfaa2e463422ba469578a77b745745a9f3b866
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2022-09-03 19:59:15 +0200

    personnal preferences

--- apps/global_preferences.h
+++ apps/global_preferences.h
@@ -39,13 +39,13 @@ private:
   static_assert(I18n::NumberOfLanguages > 0, "I18n::NumberOfLanguages is not superior to 0"); // There should already have been an error when processing an empty EPSILON_I18N flag
   static_assert(I18n::NumberOfCountries > 0, "I18n::NumberOfCountries is not superior to 0"); // There should already have been an error when processing an empty EPSILON_COUNTRIES flag
   GlobalPreferences() :
-    m_language((I18n::Language)0),
+    m_language((I18n::Language)1),
     m_country((I18n::Country)0),
     m_examMode(ExamMode::Unknown),
     m_tempExamMode(ExamMode::Standard),
     m_showPopUp(true),
-    m_brightnessLevel(Ion::Backlight::MaxBrightness),
-    m_font(KDFont::LargeFont) {}
+    m_brightnessLevel(20),
+    m_font(KDFont::SmallFont) {}
   I18n::Language m_language;
   I18n::Country m_country;
   static_assert((int8_t)GlobalPreferences::ExamMode::Off == 0, "GlobalPreferences::isInExamMode() is not right");
